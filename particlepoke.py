05b51a1f1ffb9e13e837a47d43928b788325d1a4
ttshizD <17532039+ttshiz@users.noreply.github.com>
2026-06-11 19:13:56 -0400

    initial sketch of program

---
+++ particlepoke.py
@@ -0,0 +1,36 @@
+import curses
+from curses import wrapper
+from curses.textpad import rectangle
+
+class GameOfLife(object):
+    # size of board
+    size = 23
+    board = [[None]*size]*size
+    def __init__(self):
+        return
+    def life_step():
+        return
+
+def main(stdscr):
+    stdscr.clear()
+    #stdscr.nodelay(True)
+    # make window with boarder for game, window size then coordinates
+    #win = curses.newwin(77, 77, 2, 2)
+    # boarder of window coordinates then window size
+    rectangle(stdscr, 1, 1, 23, 23)
+    #loop for game
+    for i in range(2, 10):
+        v = i-10
+        # change to add line of array
+        stdscr.addstr(i, 2, '10 divided by {} is {}'.format(v, 10/v))
+        #move refresh till after all array added
+        stdscr.refresh()
+        # check for user input
+        try:
+            keypress = stdscr.getkey()
+        except:
+            keypress = None
+        # handle input
+        #if position poke position elif start/stop do that else if quit quit
+
+wrapper(main)
