db7396395ed3c1bd680b8a7939d127363a67e111
ttshizD <17532039+ttshiz@users.noreply.github.com>
2026-06-11 23:09:20 -0400

    before switching to numpy for built in slicing

--- gameoflife.py
+++ gameoflife.py
@@ -1,17 +1,74 @@
+import numpy as np
+
 class GameOfLife:
     """ a mini game of Conway's Game Life """
     def __init__(self):
         """ Initializes a game of Conway's Game of Life on a finite playing
-        field.
+        field, using dead edges.
         """
         self.bsize = 23
-        self.board = [[None]*self.bsize]*self.bsize
-        return
+        self.board = np.zeros((self.bsize, self.bsize))
 
     def print_board(self):
         """ Prints a representation of the board """
+        print(self.board)
+
+    def num_neighbors(self, x, y):
+        """ Calculates the number of neighbors a given tile has """
+        neighbors = 0
+        if x == 0:
+            if y == 0:
+                #check only right and down
+                neighbors += self.board[x+1][y]
+                neighbors += self.board[x+1][y+1]
+                neighbors += self.board[x][y+1]
+            if y == 22: # only check left and down
+                neighbors += self.board[x+1][y]
+                neighbors += self.board[x+1][y-1]
+                neighbors += self.board[x][y-1]
+            else:
+                neighbors += self.board[x-1][y-1]
+                neighbors += self.board[x-1][y]
+                neighbors += self.board[x-1][y+1]
+                neighbors += self.board[x][y+1]
+                neighbors += self.board[x][y-1]
+                neighbors += self.board[x+1][y-1]
+                neighbors += self.board[x+1][y]
+                neighbors += self.board[x+1][y+1]
+        elif x == 22: # only check up
+            if y == 0:
+                #check only right and down
+                neighbors += self.board[x-1][y]
+                neighbors += self.board[x-1][y+1]
+                neighbors += self.board[x][y+1]
+            if y == 22: # only check left and down
+                neighbors += self.board[x-1][y]
+                neighbors += self.board[x-1][y-1]
+                neighbors += self.board[x][y-1]
+            else:
+                neighbors += self.board[x-1][y-1]
+                neighbors += self.board[x-1][y]
+                neighbors += self.board[x-1][y+1]
+                neighbors += self.board[x][y+1]
+                neighbors += self.board[x][y-1]
+                neighbors += self.board[x+1][y-1]
+                neighbors += self.board[x+1][y]
+                neighbors += self.board[x+1][y+1]
+        else:
+            neighbors += self.board[x-1][y-1]
+            neighbors += self.board[x-1][y]
+            neighbors += self.board[x-1][y+1]
+            neighbors += self.board[x][y+1]
+            neighbors += self.board[x][y-1]
+            neighbors += self.board[x+1][y-1]
+            neighbors += self.board[x+1][y]
+            neighbors += self.board[x+1][y+1]
+        return neighbors
+
+    def print_neighbors(self):
         for i in self.board:
-            print(i)
-        
+            for j in i:
+                print(self.num_neighbors(i,j))
+                
     def life_step():
         return
